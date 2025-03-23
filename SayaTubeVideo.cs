40a95625aa69cb0ead1de3c5e13e13e31653f19f
Achmadfadjry <achmadfadjryadlilfaqih@gmail.com>
2025-03-24 04:11:56 +0700

    Menambahkan class SayaTubeVideo

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD_06_103022300159
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (title == null || title.Length > 100)
+            {
+                throw new ArgumentException("Judul video tidak boleh null dan maksimal 100 karakter.");
+            }
+
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count > 10000000)
+            {
+                throw new ArgumentException("Penambahan play count maksimal 10.000.000 per panggilan");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Overflow terjadi pada penambahan play count.");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayaTubeVideo videos = new SayaTubeVideo("Tutorial Design By Contract - Achmad fadjry adlil faqih");
+            for (int i = 0; i < 3; i++)
+            {
+                videos.IncreasePlayCount(10000000);
+                videos.PrintVideoDetails();
+            }
+
+
+            try
+            {
+                SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Achmad fadjry adlil faqih");
+                video.IncreasePlayCount(10000001);
+                video.PrintVideoDetails();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
--- obj/Debug/net8.0/TP_MOD_06_103022300159.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP_MOD_06_103022300159.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MOD_06_103022300159")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e057082c0dcf681bb68a6e199f2f11adb0e5970")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MOD_06_103022300159")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MOD_06_103022300159")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
